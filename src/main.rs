479025cddca85a467c1b3271177592a6fbee09cf
Brian Matheson <bmath@brianmatheson.com>
2025-06-04 12:02:27 -0400

    working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use rcgen::{generate_simple_self_signed, CertifiedKey};
+use std::env;
 use std::fs::File;
 use std::io::Write;
-use rcgen::{generate_simple_self_signed, CertifiedKey};
 
 fn new() -> std::io::Result<()> {
     // Generate a certificate that's valid for "localhost" and "hello.world.example"
+    
     let subject_alt_names = vec!["CA".to_string()];
     
     let CertifiedKey { cert, key_pair } = generate_simple_self_signed(subject_alt_names).unwrap();
@@ -16,7 +18,30 @@ fn new() -> std::io::Result<()> {
     Ok(result)
 }
 
+fn server(args: Vec<String>) -> std::io::Result<()> {
+    Ok(())
+}
+fn client(args: Vec<String>) -> std::io::Result<()> {
+    Ok(())
+}
+fn sign(args: Vec<String>) -> std::io::Result<()> {
+    Ok(())
+}
+fn usage() -> std::io::Result<()> {
+    println!("Usage: ca new|server NAME|client NAME|sign FILENAME");
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
-    new()
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    println!("{command}");
+    
+    match command.as_str() {
+	"new" => new(),
+	"server" => server(args),
+	"client" => client(args),
+	"sign" => sign(args),
+	&_ => usage()
+    }
 }
